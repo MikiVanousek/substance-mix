70ae90cf928f818a8f1c12d743b0052830a83e81
Mikuláš Vanoušek <vanousekmikulas@gmail.com>
2021-07-14 15:23:58 +0200

    Renamed the app

---
+++ android/app/src/main/kotlin/com/vanousek/substance_mix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vanousek.substance_mix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/cz/vanousek/drug_combos/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package cz.vanousek.drug_combos
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
